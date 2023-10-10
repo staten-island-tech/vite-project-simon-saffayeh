22eb183c8ecdde8ca31f6a954aa846c4c5cfd930
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-10-10 10:21:03 -0400

    sfsdf

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,1 +1,7 @@
-import './style.css'
+import './style.css';
+import data from './data/amazon_uk_shoes_dataset.json'
+console.log(data)
+/* document.querySelector<HTMLDivElement>("#test")!.innerHTML += `
+
+
+` */
