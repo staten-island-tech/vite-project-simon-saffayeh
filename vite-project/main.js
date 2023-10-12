bc79b9ce1944df5177a7ac806650ea3f073e8208
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-10-12 10:15:25 -0400

    sfds

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,48 +1,50 @@
-import './style.css';
-import data from './data/amazon_uk_shoes_dataset.json'
+import "./style.css";
+import data from "./data/amazon_uk_shoes_dataset.json";
+let total_entries = 0;
+
+let breadcrumbsList = [];
+data.forEach((i) => {
+  if (breadcrumbsList.includes(i["breadcrumbs"]) === false) {
+    breadcrumbsList.push(i["breadcrumbs"]);
+  }
+});
 
 const currentUrl = window.location.pathname;
-let breadcrumbs = currentUrl.split('/').filter(Boolean); // Split URL and remove empty elements
+let breadcrumbs = currentUrl.split("/").filter(Boolean); // Split URL and remove empty elements
 // Function to generate breadcrumbs based on the current URL or page context
 function generateBreadcrumbs() {
+  var breadcrumbsContainer = document.getElementById("breadcrumbs-list");
+  breadcrumbsContainer.innerHTML = ""; // Clear previous breadcrumbs
 
-
-    var breadcrumbsContainer = document.getElementById("breadcrumbs-list");
-    breadcrumbsContainer.innerHTML = ""; // Clear previous breadcrumbs
-
-    var breadcrumbPath = "";
-    breadcrumbs.forEach(function (breadcrumb, index) {
-        breadcrumbPath += '/' + breadcrumb;
-        var breadcrumbItem = document.createElement("li");
-        breadcrumbItem.innerHTML = '<a href="' + breadcrumbPath + '">' + breadcrumbsList[index] + '</a>';
-        breadcrumbsContainer.appendChild(breadcrumbItem);
-    });
+  var breadcrumbPath = "";
+  breadcrumbs.forEach(function (breadcrumb, index) {
+    breadcrumbPath += "/" + breadcrumb;
+    var breadcrumbItem = document.createElement("li");
+    breadcrumbItem.innerHTML =
+      '<a href="' + breadcrumbPath + '">' + breadcrumbsList[index] + "</a>";
+    breadcrumbsContainer.appendChild(breadcrumbItem);
+  });
 }
 
 // Call the function to generate breadcrumbs when the page loads
 window.onload = generateBreadcrumbs;
 
+breadcrumbs = ["Fashion Trainers"];
+let lastbreadcrumb = breadcrumbs[breadcrumbs.length - 1];
+data.forEach((i) => {
+  if (i["breadcrumbs"].includes(lastbreadcrumb)) {
+    if (total_entries < 100) {
+      total_entries += 1;
+      document.getElementById("container-cards").innerHTML += `
+            <div class = "card">
+            <h3>${i["title"]}</h3>
+            <p>${i["price"]}</p>
+            <img class = "card-img" src = "${i["images_list"]}">
+            </div>
+            `;
+    }
+  }
+});
 
-/* let breadcrumbsList = []
-data.forEach(i => {
-    let sub = i["breadcrumbs"].split(" ")
-    sub.forEach(element => {
-        if (breadcrumbsList.includes(element) === false){
-            breadcrumbsList.push(element)
-        }
-    });
-    
-});
- */
-breadcrumbs = ["Shoes/Women's"]
-data.forEach(i => {
-    let sub = i["breadcrumbs"].split(" ")
-    sub.forEach(element => {
-        //console.log(breadcrumbs.includes(element))
-        if (breadcrumbs.includes(element) === true){
-            console.log('test')
-            document.getElementById("test").innerHTML += `<h1>${i["title"]}</h1>`
-        }
-    });
-    
-});
+
+NOTE fix how to break the loop hwen it is over instead of just keep cycleing
