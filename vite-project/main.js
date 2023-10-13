0ac0b8f096500528cd2a7c4281bbc83e83de2940
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-10-13 10:12:39 -0400

    sdfsdfsdf

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,8 +1,10 @@
 import "./style.css";
 import data from "./data/amazon_uk_shoes_dataset.json";
 let total_entries = 0;
-
+let max_entries = 25;
 let breadcrumbsList = [];
+let current_entries = 0;
+let final_entries = 0
 data.forEach((i) => {
   if (breadcrumbsList.includes(i["breadcrumbs"]) === false) {
     breadcrumbsList.push(i["breadcrumbs"]);
@@ -32,19 +34,63 @@ window.onload = generateBreadcrumbs;
 breadcrumbs = ["Fashion Trainers"];
 let lastbreadcrumb = breadcrumbs[breadcrumbs.length - 1];
 data.forEach((i) => {
-  if (i["breadcrumbs"].includes(lastbreadcrumb)) {
-    if (total_entries < 100) {
+  if(i["breadcrumbs"].includes(lastbreadcrumb)){
+    console.log(i["asin"])
+    console.log(final_entries)
+  }
+  if (i["breadcrumbs"].includes(lastbreadcrumb) && total_entries < max_entries) {
       total_entries += 1;
       document.getElementById("container-cards").innerHTML += `
             <div class = "card">
             <h3>${i["title"]}</h3>
-            <p>${i["price"]}</p>
+            <p>${i["asin"]}</p>
             <img class = "card-img" src = "${i["images_list"]}">
             </div>
             `;
+  }
+});
+/* 
+for (let i = 0; i < data.length; i++) {
+  if (data[i]["breadcrumbs"].includes(lastbreadcrumb)  && total_entries < 10 ) {
+    console.log("there")
+    total_entries += 1;
+    document.getElementById("container-cards").innerHTML += `
+      <div class="card">
+        <h3>${data[i]["title"]}</h3>
+        <p>${data[i]["price"]}</p>
+        <img class="card-img" src="${data[i]["images_list"]}">
+      </div>`;
+  } else {
+    break;
+  }
+}
+ */
+// then make it not do everything and reload more at bottom of page
+function loadMore(){
+  max_entries += 25
+  current_entries = 0;
+  data.forEach((i) => {
+    if(i["breadcrumbs"].includes(lastbreadcrumb)){
+      current_entries += 1
+      console.log(current_entries)
     }
+    if (i["breadcrumbs"].includes(lastbreadcrumb) && max_entries > total_entries && current_entries >= total_entries && total_entries <= final_entries) {
+        total_entries += 1;
+        document.getElementById("container-cards").innerHTML += `
+              <div class = "card">
+              <h3>${i["title"]}</h3>
+              <p>${i["asin"]}</p>
+              <img class = "card-img" src = "${i["images_list"]}">
+              </div>
+              `;
+    }
+  });
+}
+
+$(window).scroll(function() {
+  if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      loadMore()
   }
 });
 
-
-NOTE fix how to break the loop hwen it is over instead of just keep cycleing
+//make it so i can see earlier breadcrumbs ex. animals/dogs, animals should still displayt something
