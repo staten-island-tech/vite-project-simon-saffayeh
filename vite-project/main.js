a31830adbfaf5ab979ac80780d605da2cb4c8f2b
simon-saffayeh <93934522+simon-saffayeh@users.noreply.github.com>
2023-10-20 10:15:40 -0400

    sdf
    sdfsdfdsfgsf

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,7 @@
 import "./style.css";
 import data from "./data/amazon_uk_shoes_dataset.json";
 let total_entries = 0;
-let max_entries = 25;
+let max_entries = 3;
 let breadcrumbsList = [];
 let current_entries = 0;
 let final_entries = 0
@@ -12,7 +12,7 @@ data.forEach((i) => {
 });
 
 
-breadcrumbs = ["Shoes"];
+let breadcrumbs = ["Shoes"];
 let lastbreadcrumb = breadcrumbs[breadcrumbs.length - 1];
 
 data.forEach((i) => {
@@ -31,28 +31,7 @@ data.forEach((i) => {
   }
 });
 
-function loadMore(){
-  max_entries += 25
-  current_entries = 0;
-  data.forEach((i) => {
-    if(i["breadcrumbs"].includes(lastbreadcrumb)){
-      current_entries += 1
 
-    }
-    if (i["breadcrumbs"].includes(lastbreadcrumb) && max_entries >= total_entries && current_entries >= total_entries && total_entries <= final_entries) {
-        total_entries += 1;
-
-
-        document.getElementById("container-cards").innerHTML += `
-              <div class = "card">
-              <h3>${i["title"]}</h3>
-              <p>${i["asin"]}</p>
-              <img class = "card-img" src = "${i["images_list"]}">
-              </div>
-              `;
-    }
-  });
-}
 //make it so it gets rid of duplicates
 //make it so it doiesnt load doc until everything is loaded
 //why is shop.js loading
